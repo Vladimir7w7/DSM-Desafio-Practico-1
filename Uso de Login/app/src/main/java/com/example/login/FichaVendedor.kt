e461149c15fa2cc714d1f2a3a01264f478fe58e6
Vladimir7w7 <107089945+Vladimir7w7@users.noreply.github.com>
2023-02-26 19:16:24 -0600

    update icon

--- Uso de Login/app/src/main/java/com/example/login/FichaVendedor.kt
+++ Uso de Login/app/src/main/java/com/example/login/FichaVendedor.kt
@@ -1,14 +1,16 @@
 package com.example.login
 
 import android.app.DatePickerDialog
+import android.content.ContentValues
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import java.util.Calendar
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
 
 class FichaVendedor : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,58 +45,73 @@ class FichaVendedor : AppCompatActivity() {
 
     private fun calculo() {
 
+
         //capturando el nombre, codigo y total de ventas
 
         val camponombre: EditText = findViewById(R.id.edinombre)
         val campocod: EditText = findViewById(R.id.editcodigo)
         val campoventa: EditText = findViewById(R.id.editventa)
+        //----------------------------------------------------------------
 
-
-
-
-
-        val nombre: String = camponombre.text.toString()
-        val cod: String = campocod.text.toString()
-        val ventas: Double = campoventa.text.toString().toDouble()
-        val ventas1: String = campoventa.text.toString()
-
-
-        var result = "0"
-
-        if (ventas >= 500 && ventas <= 1000) {
-            var comi = 0.1
-
-            var total: Double = (ventas * comi)
-            result = "$total"
+        if (camponombre.getText().toString().isEmpty() || campocod.getText().toString()
+                .isEmpty() || campoventa.getText().toString().isEmpty()
+        ) {
+            Toast.makeText(this, "LLene todos los campos", Toast.LENGTH_LONG).show()
         }
-        if (ventas >= 2000 && ventas <= 3000) {
-            var comi2 = 0.15
-            val total: Double = (ventas * comi2)
-            result = "$total"
-        }
-        if (ventas >= 3000 && ventas <= 4000) {
-            val total: Double = (ventas * 0.20)
-            result = "$total"
-        }
-        if (ventas >= 4000) {
-            val total: Double = (ventas * 0.30)
-            result = "$total"
-        }
-        //---------abre activity y envia datos almacenados---------------------------------------------
+            else{
+                val nombre: String = camponombre.text.toString()
+                val cod: String = campocod.text.toString()
+                val ventas: Double = campoventa.text.toString().toDouble()
+                val ventas1: String = campoventa.text.toString()
 
 
-        val intento1 = Intent(this, ResultadoVendedor::class.java)
-        intento1.putExtra("res", nombre)
-        intento1.putExtra("res1", cod)
-        intento1.putExtra("res2", ventas1)
-        intento1.putExtra("res3", result)
-        startActivity(intento1)
 
-        //------------------------------------------------------
-        Toast.makeText(this, "CALCULADO", Toast.LENGTH_LONG).show()
+                var result = "0"
+
+                if (ventas >= 500 && ventas <= 1000) {
+                    var comi = 0.1
+
+                    var total: Double = (ventas * comi)
+                    result = "$total"
+                }
+                if (ventas >= 2000 && ventas <= 3000) {
+                    var comi2 = 0.15
+                    val total: Double = (ventas * comi2)
+                    result = "$total"
+                }
+                if (ventas >= 3000 && ventas <= 4000) {
+                    val total: Double = (ventas * 0.20)
+                    result = "$total"
+                }
+                if (ventas >= 4000) {
+                    val total: Double = (ventas * 0.30)
+                    result = "$total"
+                }
+                //---------abre activity y envia datos almacenados---------------------------------------------
+
+
+                val intento1 = Intent(this, ResultadoVendedor::class.java)
+                intento1.putExtra("res", nombre)
+                intento1.putExtra("res1", cod)
+                intento1.putExtra("res2", ventas1)
+                intento1.putExtra("res3", result)
+                startActivity(intento1)
+
+                //------------------------------------------------------
+                Toast.makeText(this, "CALCULADO", Toast.LENGTH_LONG).show()
+
+
+            }
+        }
+
+        //-----------------------------------------------------------------
+
+
+
+
 
     }
-}
+
 
 
 
--- Uso de Login/app/src/main/java/com/example/login/FormulaCuadratica.kt
+++ Uso de Login/app/src/main/java/com/example/login/FormulaCuadratica.kt
@@ -20,36 +20,43 @@ class FormulaCuadratica : AppCompatActivity() {
         val btnrespuesta = findViewById<Button>(R.id.btnsumar)
         btnrespuesta.setOnClickListener {
             // val prueba = (a.text.toString().toDouble()) * ( c.text.toString().toDouble())
-            val n = Math.pow((b.text.toString().toDouble()), (exp.toString().toDouble()))
-            val n2 = (n - ((4) * (a.text.toString().toDouble()) * (c.text.toString().toDouble())))
-            val n1 = Math.sqrt(n2)
-            val n3 = (-b.text.toString().toDouble()) + (n1.toString().toDouble())
-            val n4 = (-b.text.toString().toDouble()) - (n1.toString().toDouble())
-            val n5 = (n3) / (2 * (a.text.toString().toDouble()))
-            val n6 = n4 / (2 * (a.text.toString().toDouble()))
-
-
-
-
-            if (n2 > 0) {
-                r.setText(
-                    "Valor de x1: " + (Math.round(n5 * 1000.0) / 1000.0)
-                )
-                r2.setText(
-                    "Valor de x2: " + (Math.round(n6 * 1000.0) / 1000.0)
-                )
+            if (a.getText().toString().isEmpty() || b.getText().toString()
+                    .isEmpty() || c.getText().toString().isEmpty()
+            ) {
+                Toast.makeText(this, "LLene todos los campos", Toast.LENGTH_LONG).show()
             }
-            else
-            {
-                val text = "No existe solución"
-                val duration = Toast.LENGTH_SHORT
+            else{
+                val n = Math.pow((b.text.toString().toDouble()), (exp.toString().toDouble()))
+                val n2 = (n - ((4) * (a.text.toString().toDouble()) * (c.text.toString().toDouble())))
+                val n1 = Math.sqrt(n2)
+                val n3 = (-b.text.toString().toDouble()) + (n1.toString().toDouble())
+                val n4 = (-b.text.toString().toDouble()) - (n1.toString().toDouble())
+                val n5 = (n3) / (2 * (a.text.toString().toDouble()))
+                val n6 = n4 / (2 * (a.text.toString().toDouble()))
 
-                val toast = Toast.makeText(applicationContext, text, duration)
-                toast.show()
+
+
+
+                if (n2 > 0) {
+                    r.setText(
+                        "Valor de x1: " + (Math.round(n5 * 1000.0) / 1000.0)
+                    )
+                    r2.setText(
+                        "Valor de x2: " + (Math.round(n6 * 1000.0) / 1000.0)
+                    )
+                }
+                else
+                {
+                    val text = "No existe solución"
+                    val duration = Toast.LENGTH_SHORT
+
+                    val toast = Toast.makeText(applicationContext, text, duration)
+                    toast.show()
+
+                }
 
             }
-
-        }
+            }
 
     }
 }
